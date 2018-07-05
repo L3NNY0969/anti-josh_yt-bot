8802a77700a3a226097f2da987252fa0d900870d
L3NNY0969 <35822194+L3NNY0969@users.noreply.github.com>
2018-07-04 19:59:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const bot = new (require("discord.js").Client)();
 const prefix = "aj.";
 
-bot.on("ready", () => console.log("I'm ready. Kthx"));
+bot.on("ready", () => console.log("Yea, im online now."));
 
 bot.on("message", msg => {
     if (!msg.content.startsWith(prefix.length) || msg.author.bot) return;
     const args = msg.content.slice(prefix.length).split(" ");
     const command = args.shift().toLowerCase();
-    if (command === "truth") return msg.channel.send('Josh gay. ');
+    if (command === "ping") return msg.channel.send('Pong?');
 
     }
 });
